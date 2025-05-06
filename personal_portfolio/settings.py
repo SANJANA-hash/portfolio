758748c3286161fb88d051b8a06edf369c850c4c
SANJANA-hash <sanjusanjana1765@gmail.com>
2025-05-06 16:25:17 +0530

    Set DEBUG=False and update ALLOWED_HOSTS

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2naogr)nn3*fd!*p@jrn#$+*az*=-t=11x1xsw0_9!=z)kfa@%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
