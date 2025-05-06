36a35552179e4bc7135a4a9ccb495dfcedb2152a
SANJANA-hash <sanjusanjana1765@gmail.com>
2025-05-06 16:01:13 +0530

    ready

---
+++ projects/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from projects.models import Project
+
+def project_index(request):
+    projects = Project.objects.all()
+    return render(request, 'projects/project_index.html', {'projects': projects})
+
+def project_detail(request, pk):
+    project = Project.objects.get(pk=pk)
+    return render(request, 'projects/project_detail.html', {'project': project})
+
+
